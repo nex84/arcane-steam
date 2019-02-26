88e9a7b9c7be8f91589182c822d328572761d7c9
-=NeXuS=- <schwartzmann.a@gmail.com>
2019-02-26 14:51:17 +0100

    Update PostInstallvhosts.sh

--- PostInstallvhosts.sh
+++ PostInstallvhosts.sh
@@ -4,3 +4,5 @@ sudo chmod 755 /var/log/httpd/
 sudo chmod o+r /var/log/httpd/*
 
 sudo service httpd restart
+
+certbot --apache -d arcane-steam.com -d www.arcane-steam.com -m schwartzmann.a@gmail.com --no-eff-email --agree-tos --keep --expand --no-redirect --no-self-upgrade
