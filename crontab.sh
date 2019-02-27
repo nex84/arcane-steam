8d48beae13fbe0a48190d682e0e5324deff40218
-=NeXuS=- <schwartzmann.a@gmail.com>
2019-02-27 10:25:35 +0100

    Update crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "0 1,13 * * * root python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew --no-self-upgrade" > /etc/cron.d/lets_encrypt
-echo "0 2,14 * * * root if [ -e /etc/letsencrypt/live/arcane-steam.com/fullchain.pem ] ; then aws s3 cp /etc/letsencrypt/live/arcane-steam.com/fullchain.pem s3://nex84-admin-s3/pki/arcane-steam.com.crt"  > /etc/cron.d/arcane-steam_pki_backup
-echo "0 2,14 * * * root if [ -e /etc/letsencrypt/live/arcane-steam.com/privkey.pem ] ; then aws s3 cp /etc/letsencrypt/live/arcane-steam.com/privkey.pem s3://nex84-admin-s3/pki/arcane-steam.com.key" >> /etc/cron.d/arcane-steam_pki_backup
+echo "0 2,14 * * * root if [ -e /etc/letsencrypt/live/arcane-steam.com/fullchain.pem ] ; then aws s3 cp /etc/letsencrypt/live/arcane-steam.com/fullchain.pem s3://nex84-admin-s3/pki/arcane-steam.com.crt ; fi"  > /etc/cron.d/arcane-steam_pki_backup
+echo "0 2,14 * * * root if [ -e /etc/letsencrypt/live/arcane-steam.com/privkey.pem ] ; then aws s3 cp /etc/letsencrypt/live/arcane-steam.com/privkey.pem s3://nex84-admin-s3/pki/arcane-steam.com.key ; fi" >> /etc/cron.d/arcane-steam_pki_backup
 
 #reload cron
 #systemctl reload crond
